99ed0cf898af89dc0ce68a5c4f49d1b6b50af9d9
DutyS12345 <dutys12345@gmail.com>
2024-04-17 01:19:48 -0400

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+const printContentStyleSheet = new CSSStyleSheet();
+var printContentStyleSheetRules = {};
+document.adoptedStyleSheets = [...document.adoptedStyleSheets, printContentStyleSheet];
 for (let tabber of document.getElementsByClassName("tabber")) {
     let tabberNav;
     let tabberTabs = [];
@@ -52,70 +55,136 @@ for (let slider of document.getElementsByClassName("slider")) {
     }
 }
 
+for (let radioChoice of document.querySelectorAll('#controls input[type="radio"]')) {
+    radioChoice.onclick = () => {
+        regeneratePrintContent();
+        updateControlLayout()
+    }
+}
+
 function regeneratePrintContent() {
     loadControlsState();
     content = document.getElementById("print-content");
     content.innerHTML = "";
-
+    resetPrintContentStyles();
     switch (controlState["checklist-type"]) {
         case "dual-flex": {
-            let checklistRows = document.createElement("div");
-            checklistRows.style.display = "flex";
-            checklistRows.style.flexFlow = "column nowrap";
-            checklistRows.style.justifyContent = "space-between";
-            checklistRows.style.justifyItems = "start";
+            let checklistSecondary = document.createElement("div");
+            checklistSecondary.classList.add("checklist-secondary");
+            printContentStyleSheetRules["checklist-secondary"] = `.checklist-secondary {
+                display:flex;
+                flex-flow:column nowrap;
+                justify-content:space-between;
+                justify-item:start;
+            }`
 
             for (let rowN = 0; rowN < controlState["row-count"]; rowN++) {
-                let checklistCols = document.createElement("div");
-                checklistCols.style.display = "flex";
-                checklistCols.style.flexFlow = "row nowrap";
-                checklistCols.style.justifyContent = "space-between";
-                checklistCols.style.justifyItems = "start";
+                let checklistPrimary = document.createElement("div");
+                checklistPrimary.classList.add("checklist-primary");
+                printContentStyleSheetRules["checklist-primary"] = `.checklist-primary {
+                    display:flex;
+                    flex-flow:row nowrap;
+                    justify-content:space-between;
+                    justify-item:start;
+                }`
                 for (let colN = 0; colN < controlState["col-count"]; colN++) {
                     let checkBox = document.createElement("div");
                     checkBox.classList.add("checkbox");
-                    checklistCols.appendChild(checkBox);
+                    checklistPrimary.appendChild(checkBox);
                 }
-                checklistRows.appendChild(checklistCols);
+                checklistSecondary.appendChild(checklistPrimary);
             }
-            content.appendChild(checklistRows);
+            content.appendChild(checklistSecondary);
             break;
         }
         case "grid": {
-            var checklistRows = document.createElement("div");
-            checklistRows.style.display = "grid";
-            checklistRows.style.gridTemplate = `auto ${controlState["row-count"] > 1 ? `repeat(${controlState["row-count"] - 1}, auto) ` : ''}/ auto ${controlState["col-count"] > 1 ? `repeat(${controlState["col-count"] - 1}, 1fr auto)` : ''}`;
-            checklistRows.style.justifyContent = "space-between";
-            checklistRows.style.justifyItems = "start";
-            checklistRows.style.rowGap = `${controlState["vspacing"]}px`;
-            totalCheckBoxCount = controlState["row-count"] * controlState["col-count"];
-            for (let checkN = 0; checkN < totalCheckBoxCount; checkN++) {
-                let checkBox = document.createElement("div");
-                checkBox.classList.add("checkbox");
-                if (checkN % controlState["col-count"] != 0) {
-                    checklistRows.appendChild(document.createElement("div"))
+            switch (controlState["checkbox-type"]) {
+                case "fixed-rect": {
+                    let checklistGrid = document.createElement("div");
+                    checklistGrid.classList.add("checklist-grid");
+                    printContentStyleSheetRules["checklist-grid"] = `.checklist-grid {
+                        display:grid;
+                        grid-template:auto ${controlState["row-count"] > 1 ? `repeat(${controlState["row-count"] - 1}, auto) ` : ''}/ auto ${controlState["col-count"] > 1 ? `repeat(${controlState["col-count"] - 1}, 1fr auto)` : ''};
+                        justify-content:space-between;
+                        justify-items:start;
+                        row-gap:${controlState["vspacing"]}px;
+                    }`
+                    totalCheckBoxCount = controlState["row-count"] * controlState["col-count"];
+                    for (let checkN = 0; checkN < totalCheckBoxCount; checkN++) {
+                        let checkBox = document.createElement("div");
+                        checkBox.classList.add("checkbox");
+                        printContentStyleSheetRules["checkbox"] = `.checkbox {
+                            width:${controlState["checkbox-width"]}px;
+                            height:${controlState["checkbox-height"]}px;
+                            border:2px solid black;
+                        }`
+                        if (checkN % controlState["col-count"] != 0) {
+                            checklistGrid.appendChild(document.createElement("div"))
+                        }
+                        checklistGrid.appendChild(checkBox);
+                    }
+                    content.appendChild(checklistGrid);
+                    break;
                 }
-
-                checklistRows.appendChild(checkBox);
-
+                case "wide-rect": {
+                    let checklistSecondary = document.createElement("div");
+                    checklistSecondary.classList.add("checklist-secondary");
+                    printContentStyleSheetRules["checklist-secondary"] = `.checklist-secondary {
+                        display: grid;
+                        grid-template: auto ${controlState["row-count"] > 1 ? `repeat(${controlState["row-count"] - 1}, auto) ` : ''}/ auto;
+                        justify-items:stretch;
+                        row-gap:${controlState["vspacing"]}px;
+                    }`
+                    for (let checkRow = 0; checkRow < controlState["row-count"]; checkRow++) {
+                        let checklistPrimary = document.createElement("table");
+                        checklistPrimary.classList.add("checklist-primary");
+                        printContentStyleSheetRules["checklist-primary"] = `.checklist-primary{
+                            border-collapse:collapse;
+                            border:2px solid black;
+                        }`
+                        let checklistPrimaryInner = document.createElement("tr");
+                        checklistPrimary.appendChild(checklistPrimaryInner);
+                        for (let checkCol = 0; checkCol < controlState["col-count"]; checkCol++) {
+                            let checkBox = document.createElement("td");
+                            checkBox.classList.add("checkbox")
+                            printContentStyleSheetRules["checkbox"] = `.checkbox {
+                                width:${controlState["checkbox-width"]}px;
+                                height:${controlState["checkbox-height"]}px;
+                                border:2px solid black;
+                            }`
+                            checklistPrimaryInner.appendChild(checkBox);
+                        }
+                        checklistSecondary.appendChild(checklistPrimary);
+                    }
+                    content.appendChild(checklistSecondary);
+                    break;
+                }
+                case "tall-rect": {
+                    break;
+                }
+                default: {
+                    break;
+                }
             }
-            content.appendChild(checklistRows);
             break;
         }
     }
+    updatePrintContentStyles();
 }
-
 
 var controlState = {}
 
-const controlIds = ["col-count", "row-count", "hspacing", "vspacing"]
+const sliderControlIds = ["col-count", "row-count", "hspacing", "vspacing", "checkbox-width", "checkbox-height"];
+const radioGroupIds = ["checklist-type", "checkbox-type", "direction"];
 function loadControlsState() {
     controlState = {}
     let controls = document.getElementById("controls");
-    for (controlId of controlIds) {
+    for (controlId of sliderControlIds) {
         loadControlValue(controls, controlId);
     }
-    controlState["checklist-type"] = controls.querySelector('input[name=checklist-type]:checked').value;
+    for (let radioGroup of radioGroupIds) {
+        controlState[radioGroup] = controls.querySelector(`input[name="${radioGroup}"]:checked`).value;
+    }
     console.log(controlState)
 }
 
@@ -127,13 +196,36 @@ regeneratePrintContent()
 
 function updateControlLayout() {
     let controls = document.getElementById("controls");
-    controls.querySelectorAll(".control-group").display = true;
     switch (controlState["checklist-type"]) {
-        case "grid":
-            {
-                controls.querySelector("#hspacing-group").display = false;
-                break;
-            }
+        case "grid": {
+            controls.querySelector("#hspacing-group").style.display = "none";
+            controls.querySelector("#vspacing-group").style.display = "block";
+            break;
+        }
+        case "dual-flex": {
+            controls.querySelector("#hspacing-group").style.display = "none";
+            controls.querySelector("#vspacing-group").style.display = "none";
+            break;
+        }
+        default: {
+            controls.querySelector("#hspacing-group").style.display = "block";
+            controls.querySelector("#vspacing-group").style.display = "block";
+            break;
+        }
     }
 }
 updateControlLayout()
+
+function resetPrintContentStyles() {
+    delete printContentStyleSheetRules;
+    printContentStyleSheetRules = {};
+}
+function updatePrintContentStyles() {
+    var newRules = Object.values(printContentStyleSheetRules).filter((rule) => rule !== undefined).join(' ')
+    newRules = newRules.replaceAll(/\n\s*/g, "");
+    console.log(newRules);
+    printContentStyleSheet.replaceSync(newRules);
+}
+
+// check if array Array.isArray(json[key])
+// check if object typeof json[key] === 'object'
